5f7f83f25958ad266d40a7f92638d246556e5e8b
NathaliaFernandes28 <oliveiranfernandes@gmail.com>
2023-07-29 09:59:31 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+function saveHouse() {
+  let area = document.querySelector("input[name='area']").value;
+  let number = document.querySelector("input[name='number']").value;
+  let neighborhood = document.querySelector("input[name='neighborhood']").value;
+  let city = document.querySelector("input[name='city']").value;
+
+  let newListValue = document.createElement("li");
+  newListValue.innerText =
+    "Informações da casa adicionada: " +
+    "\n\nÁrea: " +
+    area +
+    " m " +
+    "\n\nNúmero: " +
+    number +
+    "\n\nBairro: " +
+    neighborhood +
+    "\n\nCidade: " +
+    city;
+
+  let removeButton = document.createElement("button");
+  removeButton.type = "button ";
+  removeButton.innerText = "Remover";
+  removeButton.setAttribute("onclick", "removeHouse(this)");
+
+  newListValue.appendChild(removeButton);
+  document.getElementById("house-list").appendChild(newListValue);
+}
+
+function removeHouse(button) {
+  let liToRemove = button.parentNode;
+  document.getElementById("house-list").removeChild(liToRemove);
+}
